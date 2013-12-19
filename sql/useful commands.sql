ab7cacc5dc40e4a6a7a1f90f0ff8d6d8a6b73d00
Patrick de Boer <pdeboer@mit.edu>
2013-12-19 13:13:16 -0500

    text changes and copy-data sql script

--- sql/useful commands.sql
+++ sql/useful commands.sql
@@ -1,2 +1,18 @@
+set @nextround=4, @prevround=3;
+
 -- transition codes to next round
-update codes set round_id=4 where code not in (select code from users);
+update codes set round_id=@nextround where code not in (select code from users);
+
+-- copy data to next round
+insert into  global (id, round_id, user_id, name, create_date, last_modification, origin_round)
+select @nextround*id, @nextround as round_id, user_id, name, create_date, last_modification, round_id as origin
+from global where round_id=@prevround;
+
+insert into part (id, user_id, round_id, template_part_id, name, body, create_date, last_modification)
+select id*@nextround, user_id, @nextround, template_part_id, name, body, create_date, last_modification
+from part where round_id=@prevround;
+
+insert into global_parts (global_id, part_id)
+select global_id*@nextround, part_id*@nextround
+from global_parts gp inner join global g on gp.global_id=g.id
+where g.round_id=@prevround;
